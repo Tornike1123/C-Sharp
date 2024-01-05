880db710540ccf60a9793f75cfcebc9642de3e2a
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-05 12:28:47 +0400

    return statement

--- project1/project1/Method.cs
+++ project1/project1/Method.cs
@@ -4,15 +4,29 @@ class Method
 	{
 		static void Main (string[] args)
 		{
+			//Call the method
+			Console.WriteLine(cube(5));
+
 			SayHi(11, "toko");
             SayHi(12, "gio");
             SayHi(14, "ragac");
 
             Console.ReadLine();
+		
 		}
+
+		//method
 		static void SayHi(int num, string str)
 		{
 			Console.WriteLine("hello " + num + " " + str);
+		}
+
+
+		//return statement
+		static int cube(int numb)
+		{
+			int result = numb * numb * numb;
+			return result;
 		}
 	}
 }
