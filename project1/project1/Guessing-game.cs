623f8e13ed7493d55a190bd301f74b5cfa75349d
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-08 17:22:06 +0400

    guessing game

---
+++ project1/project1/Guessing-game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace project1
+
+
+{
+	public class Guessing_game
+	{
+		static void Main(string[] args)
+		{
+			string secretWord = "girrafe";
+			string guess = "";
+			int guessCount = 0;
+			int guessLimit = 3;
+			bool outOfGuesses = false;
+
+			while (guess != secretWord && !outOfGuesses)
+			{
+				if (guessCount < guessLimit)
+				{
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+				else
+				{
+					outOfGuesses = true;
+
+                }
+			}
+			if (outOfGuesses)
+			{
+				Console.WriteLine("You Lose!");
+			}
+			else
+			{
+                Console.WriteLine("You Win!");
+
+            }
+
+			Console.ReadLine();
+
+
+        }
+	}
+}
+
+
+
--- project1/project1/Method.cs
+++ project1/project1/Method.cs
@@ -1,33 +1,33 @@
-﻿namespace project1
-{
-class Method
-	{
-		static void Main (string[] args)
-		{
-			//Call the method
-			Console.WriteLine(cube(5));
+﻿//namespace project1
+//{
+//class Method
+//	{
+//		static void Main (string[] args)
+//		{
+//			//Call the method
+//			Console.WriteLine(cube(5));
 
-			SayHi(11, "toko");
-            SayHi(12, "gio");
-            SayHi(14, "ragac");
+//			SayHi(11, "toko");
+//            SayHi(12, "gio");
+//            SayHi(14, "ragac");
 
-            Console.ReadLine();
+//            Console.ReadLine();
 		
-		}
+//		}
 
-		//method
-		static void SayHi(int num, string str)
-		{
-			Console.WriteLine("hello " + num + " " + str);
-		}
+//		//method
+//		static void SayHi(int num, string str)
+//		{
+//			Console.WriteLine("hello " + num + " " + str);
+//		}
 
 
-		//return statement
-		static int cube(int numb)
-		{
-			int result = numb * numb * numb;
-			return result;
-		}
-	}
-}
+//		//return statement
+//		static int cube(int numb)
+//		{
+//			int result = numb * numb * numb;
+//			return result;
+//		}
+//	}
+//}
 
