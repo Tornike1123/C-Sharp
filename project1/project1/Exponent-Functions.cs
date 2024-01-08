263e4482eedeffe8a5b336fbf4beb013f00ef7c8
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-08 18:37:31 +0400

    Exponent function

---
+++ project1/project1/Exponent-Functions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace project1
+{
+	public class Exponent_Functions
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetPow(3,3));
+			Console.ReadLine();
+		}
+
+
+		static int GetPow(int baseNum, int powerNum)
+		{
+			int result = 1;
+
+			for (int i = 0; i < powerNum; i++)
+			{
+				result = result * baseNum;
+			}
+
+			return result;
+		}
+
+
+	}
+}
+
--- project1/project1/For-Loop.cs
+++ project1/project1/For-Loop.cs
@@ -1,17 +1,17 @@
-﻿using System;
-namespace project1
-{
-	public class For_Loop
-	{
-		static void Main(string[] args)
-		{
-			int[] luckyNumbers = { 3, 5, 7, 11, 23, 52 };
-			for (int i = 0; i <= luckyNumbers.Length - 1; i++)
-			{
-				Console.WriteLine(luckyNumbers[i]);
-			}
-			Console.ReadLine();
-		}
-	}
-}
+﻿//using System;
+//namespace project1
+//{
+//	public class For_Loop
+//	{
+//		static void Main(string[] args)
+//		{
+//			int[] luckyNumbers = { 3, 5, 7, 11, 23, 52 };
+//			for (int i = 0; i <= luckyNumbers.Length - 1; i++)
+//			{
+//				Console.WriteLine(luckyNumbers[i]);
+//			}
+//			Console.ReadLine();
+//		}
+//	}
+//}
 
