28e3e389e30c2ef6fb9c7f6a4af6f6ef3582117e
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-08 18:08:21 +0400

    for loop

---
+++ project1/project1/For-Loop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace project1
+{
+	public class For_Loop
+	{
+		static void Main(string[] args)
+		{
+			int[] luckyNumbers = { 3, 5, 7, 11, 23, 52 };
+			for (int i = 0; i <= luckyNumbers.Length - 1; i++)
+			{
+				Console.WriteLine(luckyNumbers[i]);
+			}
+			Console.ReadLine();
+		}
+	}
+}
+
