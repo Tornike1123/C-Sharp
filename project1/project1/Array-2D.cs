b35193e508a978255e94023a37ca50f0ff5917e6
Tornike Khatiashvili <95305738+Tornike1123@users.noreply.github.com>
2024-01-09 13:56:03 +0400

    Exception handling

--- project1/project1/Array-2D.cs
+++ project1/project1/Array-2D.cs
@@ -1,23 +1,23 @@
-﻿using System;
-namespace project1
-{
-	public class Array_2D
-	{
-		static void Main(string[] args)
-		{
-			int[,] numberGrid =
-			{
-				{1,2 },
-                {3,4 },
-                {5,6 },
+﻿//using System;
+//namespace project1
+//{
+//	public class Array_2D
+//	{
+//		static void Main(string[] args)
+//		{
+//			int[,] numberGrid =
+//			{
+//				{1,2 },
+//                {3,4 },
+//                {5,6 },
 
-            };
-			foreach (int item in numberGrid)
-			{
-				Console.WriteLine(item);
-			}
-			Console.ReadLine();
-		}
-	}
-}
+//            };
+//			foreach (int item in numberGrid)
+//			{
+//				Console.WriteLine(item);
+//			}
+//			Console.ReadLine();
+//		}
+//	}
+//}
 
---
+++ project1/project1/Exception-handling.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//namespace project1
+//{
+//	public class Exception_handling
+//	{
+//		static void Main(string[] args)
+//		{
+//			try
+//			{
+//                Console.Write("Enter a number: ");
+//                int numb1 = Convert.ToInt32(Console.ReadLine());
+//                Console.Write("Enter another number: ");
+//                int numb2 = Convert.ToInt32(Console.ReadLine());
+
+//                Console.WriteLine(numb1 / numb2);
+//            }
+//            catch (Exception ex)
+//            {
+//                // all exteption error
+//                Console.WriteLine(ex.Message);
+//            }
+//            catch (DivideByZeroException ex)
+//            {
+//                // only divide by zero error
+//                Console.WriteLine(ex.Message);
+//            }
+//            catch (FormatException ex)
+//            {
+//                // only format exception
+//                Console.WriteLine(ex.Message);
+//            }
+//        }
+//	}
+//}
+
+
+//short summary: if you use Exception you cannot use (DivideByZeroException,FormatException)
